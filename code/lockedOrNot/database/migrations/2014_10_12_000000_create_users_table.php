1afc8ee993ed11a3735233c88c4235ab8ffb3f0f
Jolita Grazyte <jolita.grazyte@student.kdg.be>
2015-12-07 16:25:12 +0100

    laravel install

---
+++ code/lockedOrNot/database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('username');
+            $table->string('email')->unique();
+            $table->string('password', 60);
+
+            $table->string('name', 45);
+            $table->string('firstname', 45);
+
+            //for statistics
+            $table->string('city', 45)->nullable();
+            $table->string('car_brand', 45)->nullable();
+            $table->string('car_color', 45)->nullable();
+
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users');
+    }
+}
